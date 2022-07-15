58d330941c3d2061ca0a91a459274c01ae6da307
Sergio Castro <sj.castro1100@gmail.com>
2022-07-15 18:17:29 -0500

    create table and model user_domicilio

---
+++ database/migrations/2022_07_15_231035_create_user_domicilios_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserDomiciliosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_domicilio', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('domicilio');
+            $table->string('numero_exterior');
+            $table->string('colonia');
+            $table->string('cp');
+            $table->string('ciudad');
+
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_domicilios');
+    }
+}
